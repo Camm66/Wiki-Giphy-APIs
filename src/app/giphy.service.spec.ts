70df42457772b935659cd5e9045fb08f5962ff33
Camm66 <Cam.morales93@gmail.com>
2018-10-30 17:44:04 -0500

    test: began implementing giphy.service tests

--- src/app/giphy.service.spec.ts
+++ src/app/giphy.service.spec.ts
@@ -1,9 +1,65 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, fakeAsync, tick, TestBed } from '@angular/core/testing';
+import {MockBackend} from '@angular/http/testing';
+import { Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 
-import { GiphyService } from './giphy.service';
+import { GiphyService } from './app/giphy.service';
+
+const giphyAPIKey = 'fakeAPIkey';
 
 describe('GiphyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+     providers: [ BaseRequestOptions,
+                  MockBackend,
+                  GiphyService,
+                  { provide: Http,
+                    useFactory: (backend: ConnectionBackend,
+                                 defaultOptions: BaseRequestOptions) => {
+                                 return new Http(backend, defaultOptions); },
+                    deps: [MockBackend, BaseRequestOptions] },
+                  ]
+            });
+    });
+  );
+
+
+
+
+function expectUrl(nackend: MockBackend, url: string){
+  backend.connections.subscribe(c => {
+    expect(c.request.url).toBe(url);
+    const response = new ResponseOptions({body: '{"img_url": "http://img_url"}'});
+    c.mockRespond(new Response(response));
+  })
+}
+
+
+
+describe('search', () => {
+    it('searchs for gif based on search term',
+      inject([GiphyService, MockBackend], fakeAsync((giphyService, mockBackend) => {
+        let res;
+        expectURL(mockBackend,  `https://api.giphy.com/v1/gifs/search?api_key=${giphyAPIKey}&q=searchText&limit=5`);
+        svc.search('searchText').subscribe((_res) => {
+          res = _res;
+        });
+        tick();
+        expect(res.img_url).toBe('http://img_url');
+      }))
+    );
+
+    it('fails if missing api key',
+      inject([GiphyService, MockBackend], fakeAsync((giphyService, mockBackend) => {
+        let res;
+        expectURL(mockBackend,  `https://api.giphy.com/v1/gifs/search?api_key=q=searchText&limit=5`);
+        svc.search('searchText').subscribe((_res) => {
+          res = _res;
+        });
+        tick();
+        expect(res.img_url).toBe('404 error');
+      }))
+    );
+
+  });
 
   it('should be created', () => {
     const service: GiphyService = TestBed.get(GiphyService);
